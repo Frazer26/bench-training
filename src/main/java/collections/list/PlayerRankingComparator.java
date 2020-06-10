9a1cebe72922521e2e55f03ae36922939005150a
Frazer26 <adrian1468@gmail.com>
2020-06-10 14:41:19 +0200

    Comparable vs comparator

---
+++ src/main/java/collections/list/PlayerRankingComparator.java
@@ -0,0 +1,10 @@
+package collections.list;
+
+import java.util.Comparator;
+
+public class PlayerRankingComparator implements Comparator<Player> {
+    @Override
+    public int compare(Player firstPlayer, Player secondPlayer) {
+        return firstPlayer.getRanking() - secondPlayer.getRanking();
+    }
+}
